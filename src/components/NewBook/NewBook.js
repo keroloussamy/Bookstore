becf983652a5bde44f05d976ece18f524b94afdf
kerolous samy <keroloussamy98@gmail.com>
2022-01-20 00:38:43 +0200

    Add AddBook functino

--- src/components/NewBook/NewBook.js
+++ src/components/NewBook/NewBook.js
@@ -1,17 +1,59 @@
-import React from 'react';
+// import useDispatch hook
+import { useDispatch } from 'react-redux';
+import nextId from 'react-id-generator';
+import React, { useState } from 'react';
+// import your Action Creators
+import { addBook } from '../../redux/books/books';
 
 function NewBook() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const dispatch = useDispatch();
+
+  const AuthorHandler = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const TitleHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: nextId(),
+      title,
+      author,
+    };
+
+    // dispatch an action and pass it the newBook object (your action's payload)
+    dispatch(addBook(newBook));
+  };
+
   return (
     <form>
       <h2>ADD NEW BOOK</h2>
-      <input type="text" />
+      <input
+        type="text"
+        value={author}
+        onChange={AuthorHandler}
+        placeholder="Author"
+      />
+
+      <input
+        type="text"
+        value={title}
+        onChange={TitleHandler}
+        placeholder="Title"
+      />
       <select>
         <option value="a">Action 1</option>
         <option value="a">Action 2</option>
         <option value="a">Action 3</option>
         <option value="a">Action 4</option>
       </select>
-      <button type="submit">ADD BOOK</button>
+      <button type="submit" onClick={submitBookToStore}>ADD BOOK</button>
     </form>
   );
 }
