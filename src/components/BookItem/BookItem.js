937d7589b24ec8810cf142fdc7049159639bc89a
kerolous samy <keroloussamy98@gmail.com>
2022-01-20 00:39:31 +0200

    Display items from reducer

--- src/components/BookItem/BookItem.js
+++ src/components/BookItem/BookItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './BookItem.module.css';
 
-function BookItem() {
+function BookItem({ book }) {
   return (
     <div className={styles.bookItem}>
       <p className="itemCategory">Action</p>
-      <h2 className="itemTitle">The Hunger Games</h2>
-      <p className="itemAuthor">Frank Herbert</p>
+      <h2 className="itemTitle">{book.title}</h2>
+      <p className="itemAuthor">{book.author}</p>
       <ul className={styles.itemActions}>
         <li><button type="button" onClick="">Comments</button></li>
         <li><button type="button" onClick="">Remove</button></li>
@@ -15,5 +16,12 @@ function BookItem() {
     </div>
   );
 }
+
+BookItem.propTypes = {
+  book: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default BookItem;
--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BookItem from '../BookItem/BookItem';
 import NewBook from '../NewBook/NewBook';
 
 function Books() {
+  const books = useSelector((state) => state.booksReducer);
   return (
     <div>
-      <BookItem />
-      <BookItem />
-      <BookItem />
+      {books.map((book) => (
+        <BookItem key={book.id} book={book} />
+      ))}
       <NewBook />
     </div>
   );
