35b3ba269531ad26fb94b82cb94e74133dd2ac7d
kerolous samy <keroloussamy98@gmail.com>
2022-01-20 00:59:32 +0200

    Add Remove function

--- src/components/BookItem/BookItem.js
+++ src/components/BookItem/BookItem.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// import useDispatch hook
+import { useDispatch } from 'react-redux';
+// import your Action Creators
+import { removeBook } from '../../redux/books/books';
 import styles from './BookItem.module.css';
 
 function BookItem({ book }) {
+  const dispatch = useDispatch();
+  const removeHandler = () => {
+    dispatch(removeBook(book.id));
+  };
+
   return (
     <div className={styles.bookItem}>
       <p className="itemCategory">Action</p>
@@ -10,7 +19,7 @@ function BookItem({ book }) {
       <p className="itemAuthor">{book.author}</p>
       <ul className={styles.itemActions}>
         <li><button type="button" onClick="">Comments</button></li>
-        <li><button type="button" onClick="">Remove</button></li>
+        <li><button type="button" onClick={removeHandler}>Remove</button></li>
         <li><button type="button" onClick="">Edit</button></li>
       </ul>
     </div>
@@ -19,6 +28,7 @@ function BookItem({ book }) {
 
 BookItem.propTypes = {
   book: PropTypes.shape({
+    id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
   }).isRequired,
