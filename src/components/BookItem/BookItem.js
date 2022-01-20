ad8f8295bf350c63f3bccce6b7287621da9ce628
kerolous samy <keroloussamy98@gmail.com>
2022-01-20 20:00:59 +0200

    use remove action inside the component

--- src/components/BookItem/BookItem.js
+++ src/components/BookItem/BookItem.js
@@ -3,24 +3,21 @@ import PropTypes from 'prop-types';
 // import useDispatch hook
 import { useDispatch } from 'react-redux';
 // import your Action Creators
-import { removeBook } from '../../redux/books/books';
+import { removeBookAsync } from '../../redux/books/books';
 import styles from './BookItem.module.css';
 
 function BookItem({ book }) {
   const dispatch = useDispatch();
   const removeHandler = () => {
-    dispatch(removeBook(book.id));
+    dispatch(removeBookAsync(book.item_id));
   };
 
   return (
     <div className={styles.bookItem}>
-      <p className="itemCategory">Action</p>
+      <p className="itemCategory">{ book.category }</p>
       <h2 className="itemTitle">{book.title}</h2>
-      <p className="itemAuthor">{book.author}</p>
       <ul className={styles.itemActions}>
-        <li><button type="button" onClick="">Comments</button></li>
         <li><button type="button" onClick={removeHandler}>Remove</button></li>
-        <li><button type="button" onClick="">Edit</button></li>
       </ul>
     </div>
   );
@@ -28,9 +25,9 @@ function BookItem({ book }) {
 
 BookItem.propTypes = {
   book: PropTypes.shape({
-    id: PropTypes.string.isRequired,
+    item_id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
   }).isRequired,
 };
 
