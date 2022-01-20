754af48cc65647655e61255a7e7677098dee59d9
kerolous samy <keroloussamy98@gmail.com>
2022-01-20 18:43:09 +0200

    addBookAPI function to request api

--- src/API/book.js
+++ src/API/book.js
@@ -1,9 +1,17 @@
 const baseURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
 
-const getBooks = async () => {
+export const getBooksAPI = async () => {
   const data = await fetch(`${baseURL}/apps/o3JOPgIWvfQ0PNJonLQr/books`)
     .then((responce) => responce.json());
   return data;
 };
 
-export default getBooks;
+export const addBookAPI = async (book) => {
+  const response = await fetch(`${baseURL}/apps/o3JOPgIWvfQ0PNJonLQr/books`, {
+    method: 'POST',
+    body: JSON.stringify(book),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  });
+  const data = await response.text();
+  return data;
+};
