3173d616d36eb77255a1c77f34c27000a46df773
kerolous samy <keroloussamy98@gmail.com>
2022-01-20 18:07:41 +0200

    Add getBooks action to books reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,8 @@
+import getBooks from '../../API/book';
+
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const GET_BOOKS = 'bookStore/books/GET_BOOKS';
 const initialState = [];
 
 export const addBook = (payload) => ({
@@ -12,12 +15,24 @@ export const removeBook = (id) => ({
   id,
 });
 
+export const getBook = (payload) => ({
+  type: GET_BOOKS,
+  payload,
+});
+
+export const getBooksAsync = () => async (dispatch) => {
+  const data = await getBooks();
+  dispatch(getBook(data));
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.id);
+    case GET_BOOKS:
+      return action.payload;
     default:
       return state;
   }
